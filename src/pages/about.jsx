967875a4c6a0899a0900ea9873e86e32554f0107
Erin Jerri Pangilinan <3184381+erinjerri@users.noreply.github.com>
2022-11-07 11:43:45 -0800

    changed bio copy

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -5,8 +5,6 @@ import clsx from 'clsx'
 
 import { Container } from '@/components/Container'
 import {
-  TwitterIcon,
-  InstagramIcon,
   GitHubIcon,
   LinkedInIcon,
 } from '@/components/SocialIcons'
@@ -61,41 +59,24 @@ export default function About() {
           </div>
           <div className="lg:order-first lg:row-span-2">
             <h1 className="text-4xl font-bold tracking-tight text-zinc-800 dark:text-zinc-100 sm:text-5xl">
-              I’m Spencer Sharp. I live in New York City, where I design the
-              future.
+              I’m Erin Pangilinan.
             </h1>
             <div className="mt-6 space-y-7 text-base text-zinc-600 dark:text-zinc-400">
               <p>
-                I’ve loved making things for as long as I can remember, and
-                wrote my first program when I was 6 years old, just two weeks
-                after my mom brought home the brand new Macintosh LC 550 that I
-                taught myself to type on.
+                Enter my universe.
               </p>
               <p>
-                The only thing I loved more than computers as a kid was space.
-                When I was 8, I climbed the 40-foot oak tree at the back of our
-                yard while wearing my older sister’s motorcycle helmet, counted
-                down from three, and jumped — hoping the tree was tall enough
-                that with just a bit of momentum I’d be able to get to orbit.
+              I have been writing code and scripts since CSS was born in 1993. I'm an entrepreneur, author, as well as a software engineer and computational hybrid. 
+              I take a interdisciplinary approach to technical and creative work, both coding and designing, producing and screenwriting. 
+              My name is Erin Pangilinan (pronounced "Pong" "eel" "lee" "none". And no, my book cover of
+              Creating Augmented and Virtual Realities was not chosen by me and my last name has nothing to do with pangolins. 
               </p>
-              <p>
-                I spent the next few summers indoors working on a rocket design,
-                while I recovered from the multiple surgeries it took to fix my
-                badly broken legs. It took nine iterations, but when I was 15 I
-                sent my dad’s Blackberry into orbit and was able to transmit a
-                photo back down to our family computer from space.
-              </p>
-              <p>
-                Today, I’m the founder of Planetaria, where we’re working on
-                civilian space suits and manned shuttle kits you can assemble at
-                home so that the next generation of kids really <em>can</em>{' '}
-                make it to orbit — from the comfort of their own backyards.
-              </p>
-            </div>
+              </div>
           </div>
           <div className="lg:pl-20">
-            <ul role="list"
-              <SocialLink href="https://www.github.com/erinjerri" icon={GitHubIcon} className="mt-4">
+            <ul role="list">
+        
+            <SocialLink href="https://www.github.com/erinjerri" icon={GitHubIcon} className="mt-4">
                 Follow on GitHub
               </SocialLink>
               <SocialLink href="https://www.linkedin.com/in/erinjerri" icon={LinkedInIcon} className="mt-4">
