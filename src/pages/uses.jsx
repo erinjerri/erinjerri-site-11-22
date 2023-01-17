ae5a6d865d5be8460cf25774db50dfc88d3bbd72
Erin Jerri Pangilinan <3184381+erinjerri@users.noreply.github.com>
2023-01-17 15:50:23 -0800

    Update uses.jsx

--- src/pages/uses.jsx
+++ src/pages/uses.jsx
@@ -45,43 +45,33 @@ export default function Uses() {
               A lot faster than my last MacBook, could work better for AI, but still a huge improvement from my prior laptop.
               I like the sliding bar most.
             </Tool>
-            <Tool title="MacBook Mouse with bumpbers">
-              The essential must-haves.
+            <Tool title="MacBook Mouse with Fironst Magic Grips bumpers">
+              The essential must-have bumper to hug my Apple Macbook Mouse.
             </Tool>
             <Tool title="Automonous Chair">
               Ergonomic chairs. https://www.autonomous.ai/office-chairs
             </Tool>
-            <Tool title="Apple Magic Trackpad">
-              Something about all the gestures makes me feel like a wizard with
-              special powers. I really like feeling like a wizard with special
-              powers.
+            <Tool title="Flexispot Standing Desk">
+              Great for anyone struggling with back problems sitting for long periods of time.
             </Tool>
-            <Tool title="Herman Miller Aeron Chair">
-              If I’m going to slouch in the worst ergonomic position imaginable
-              all day, I might as well do it in an expensive chair.
+            <Tool title="Ergonomic Foam - Footrest">
+              For my feet
             </Tool>
           </ToolsSection>
           <ToolsSection title="Development tools">
-            <Tool title="Sublime Text 4">
-              I don’t care if it’s missing all of the fancy IDE features
-              everyone else relies on, Sublime Text is still the best text
-              editor ever made.
+            <Tool title="Visual Studio">
+              After going from DreamWeaver in the early 90s and 2000s, to Sublime Text, in the early 2010s, and then to Atom, it's a switch that works for all other parts of the stack outside of mobile iOS development.
             </Tool>
-            <Tool title="iTerm2">
-              I’m honestly not even sure what features I get with this that
-              aren’t just part of the macOS Terminal but it’s what I use.
+            <Tool title="Fish">
+              My skin for iTerm.
             </Tool>
-            <Tool title="TablePlus">
-              Great software for working with databases. Has saved me from
-              building about a thousand admin interfaces for my various projects
-              over the years.
+            <Tool title="Inkdrop">
+              Where I like to take notes for pretty much everything, and sometims code snippets.
             </Tool>
           </ToolsSection>
           <ToolsSection title="Design">
-            <Tool title="Figma">
-              We started using Figma as just a design tool but now it’s become
-              our virtual whiteboard for the entire company. Never would have
-              expected the collaboration features to be the real hook.
+            <Tool title="Figma and Sketch">
+              I'm an early Figma and Sketch user.
             </Tool>
           </ToolsSection>
           <ToolsSection title="Productivity">
@@ -89,21 +79,14 @@ export default function Uses() {
               It’s not the newest kid on the block but it’s still the fastest.
               The Sublime Text of the application launcher world.
             </Tool>
-            <Tool title="Reflect">
-              Using a daily notes system instead of trying to keep things
-              organized by topics has been super powerful for me. And with
-              Reflect, it’s still easy for me to keep all of that stuff
-              discoverable by topic even though all of my writing happens in the
-              daily note.
+            <Tool title="Notion">
+              Where I can do check-off lists
             </Tool>
-            <Tool title="SavvyCal">
-              Great tool for scheduling meetings while protecting my calendar
-              and making sure I still have lots of time for deep work during the
-              week.
+            <Tool title="Rainlendar">
+              For when I need to look at a calendar on my desktop.
             </Tool>
-            <Tool title="Focus">
-              Simple tool for blocking distracting websites when I need to just
-              do the work and get some momentum going.
+            <Tool title="Spectacle">
+             Must have for splitting your screen.
             </Tool>
           </ToolsSection>
         </div>
