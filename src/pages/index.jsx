a40add2dbe10501e5a722a2b830787fa8d63f33e
Erin Jerri Pangilinan <3184381+erinjerri@users.noreply.github.com>
2023-01-26 16:10:42 -0800

    updated links for social and substack

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -128,7 +128,7 @@ function Newsletter() {
           required
           className="min-w-0 flex-auto appearance-none rounded-md border border-zinc-900/10 bg-white px-3 py-[calc(theme(spacing.2)-1px)] shadow-md shadow-zinc-800/5 placeholder:text-zinc-400 focus:border-teal-500 focus:outline-none focus:ring-4 focus:ring-teal-500/10 dark:border-zinc-700 dark:bg-zinc-700/[0.15] dark:text-zinc-200 dark:placeholder:text-zinc-500 dark:focus:border-teal-400 dark:focus:ring-teal-400/10 sm:text-sm"
         />
-        <Button href="erinjerri.substack.com"><Button type="submit" className="ml-4 flex-none">
+        <Button href="https://www.erinjerri.substack.com"><Button type="submit" className="ml-4 flex-none">
         Join
         </Button></Button>
       </div>
@@ -261,22 +261,12 @@ export default function Home({ articles }) {
           </p>
           <div className="mt-6 flex gap-6">
             <SocialLink
-              href="https://twitter.com"
-              aria-label="Follow on Twitter"
-              icon={TwitterIcon}
-            />
-            <SocialLink
-              href="https://instagram."
-              aria-label="Follow on Instagram"
-              icon={InstagramIcon}
-            />
-            <SocialLink
-              href="https://github."
+              href="https://github.com/erinjerri"
               aria-label="Follow on GitHub"
               icon={GitHubIcon}
             />
             <SocialLink
-              href="https://linkedin.com"
+              href="https://linkedin.com/in/erinjerri"
               aria-label="Follow on LinkedIn"
               icon={LinkedInIcon}
             />
