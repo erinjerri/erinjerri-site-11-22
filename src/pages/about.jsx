697eb9feaac984ffe11f1a5e018ffd170f3ca96b
Erin Jerri Pangilinan <3184381+erinjerri@users.noreply.github.com>
2023-02-01 21:34:55 -0800

    changed email again

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -112,7 +112,7 @@ export default function About() {
                 icon={MailIcon}
                 className="mt-8 border-t border-zinc-100 pt-8 dark:border-zinc-700/40"
               >
-                contact@erinjerri.xyz
+                contactme@erinjerri.xyz
               </SocialLink>
             </ul>
           </div>
