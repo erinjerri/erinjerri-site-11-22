4b0151d2c422e0192a77ee1de85aafc33717759a
Erin Jerri Pangilinan <3184381+erinjerri@users.noreply.github.com>
2023-02-01 20:46:32 -0800

    updated email address

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -108,7 +108,7 @@ export default function About() {
                 Follow on LinkedIn
               </SocialLink>
               <SocialLink
-                href="mailto:contact@erinjerri.xyz"
+                href="mailto:contactme@erinjerri.xyz"
                 icon={MailIcon}
                 className="mt-8 border-t border-zinc-100 pt-8 dark:border-zinc-700/40"
               >
